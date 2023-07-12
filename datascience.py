aa30f88c50055b86804cfabc60955587ba5c44b4
Vasanth Kumar Croos <croosvasanthkumar@gmail.com>
2023-07-12 18:34:27 +0530

    Add files via upload

---
+++ datascience.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+
+# In this notebook, Data Science Tools and Ecosystem are summarized.
+
+# Objectives:
+# List popular languages for Data Science
+# Some of the commonly used libraries used by Data Scientists 
+# Data Science Tools 
+# simple arithmetic expression to mutiply then add integers.
+# This will convert 200 minutes to hours by diving by 60.
+
+# Some of the popular languages that Data Scientists use are:
+# 1.Python
+# 2.R
+# 3.SQL
+# 4.Java
+
+# Some of the commonly used libraries used by Data Scientists include:
+# 1.pandas
+# 2.Numpy
+# 3.Matplotlib
+# 
+
+# | Data Science Tools |
+# | Jupyter Notebook  |
+# | RStudio           |
+# | Spyder            |
+# 
+
+# # Below are a few examples of evaluating arithmetic expressions in Python.
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers.
+(3*4)+5
+
+
+# In[2]:
+
+
+#  This will convert 200 minutes to hours by diving by 60.
+200/60
+
+
+# # Author
+# Anthonimuththu Vasanth Kumar Croos
